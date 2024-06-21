62b0a8eacb09c4f1f69618ad0e7dac882746c01d
Anatoliy <targoniyanatoliy@gmail.com>
2024-06-21 14:43:05 +0300

    Update script.js
    add new url in fetch

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function addCards() {
     let cardsArr;
 
     // Викликаємо fetch, щоб отримати дані з файлу hotels.json
-    fetch('http://127.0.0.1:5500/data/hotels.json')
+    fetch('https://targoniyanatoliy.github.io/dz5-geolocation/data/hotels.json')
         .then(response => response.json()) // Парсимо відповідь у формат JSON
         .then(json => {
             cardsArr = json; // Присвоюємо отриманий масив даних змінній cardsArr
@@ -79,37 +79,3 @@ function removeCards() {
         }
     }
 }
-
-
-
-
-/*
-[
-    {
-        image: './img/hotel1.jpeg', 
-        title: 'White Villas', 
-        text: `2-х поверхові окремі будиночки, розташовані в одному з найзнаменитіших курортів України -Коблево`,
-        map: `https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5509.581270195974!2d31.2203655!3d46.6165453!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c66bb2bef714f1%3A0x97c7ea7cceb56b5!2z0L_RgNC-0YHQv9C10LrRgiDQmtGD0YDQvtGA0YLQvdC40LksIDYxLCDQmtC-0LHQu9C10LLQtSwg0JzQuNC60L7Qu9Cw0ZfQstGB0YzQutCwINC-0LHQu9Cw0YHRgtGMLCA1NzQ1Mw!5e1!3m2!1suk!2sua!4v1718450058292!5m2!1suk!2sua`
-    }, 
-    {
-        image: './img/hotel2.jpeg',
-        title: 'De La Vita', 
-        text: `Готель розташований на першій береговій лінії, в найпопулярнішому курорті Миколаївській
-        області-Коблево`,
-        map: `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d955.3172715364365!2d31.21346999229365!3d46.62078539316895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c66b1e13afbbbd%3A0x55783531bc4872e6!2sDe%20La%20Vita!5e1!3m2!1suk!2sua!4v1718457519454!5m2!1suk!2sua`
-    }, 
-    {
-        image: './img/hotel3.jpeg',
-        title: 'Valen Park Pension', 
-        text: `Готель розташований на першій береговій лінії, в найпопулярнішому курорті Миколаївській
-        області-Коблево`,
-        map: `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1935.309554847864!2d31.212396841762303!3d46.6210133649521!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c66baafd5b9a7f%3A0xb2a3eec7f9a1a3b4!2z0L_RgNC-0YHQv9C10LrRgiDQmtGD0YDQvtGA0YLQvdC40LksIDQxLCDQmtC-0LHQu9C10LLQtSwg0JzQuNC60L7Qu9Cw0ZfQstGB0YzQutCwINC-0LHQu9Cw0YHRgtGMLCA1NzQ1Mw!5e1!3m2!1suk!2sua!4v1718457481963!5m2!1suk!2sua`
-    }, 
-    {
-        image: './img/hotel4.jpeg', 
-        title: 'Friday Wood', 
-        text: `Готель розташований на першій береговій лінії, в найпопулярнішому курорті Миколаївській
-        області-Коблево`,
-        map: `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1359.6727055520898!2d31.212087947881255!3d46.622726408024754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c66baa79e921ef%3A0xd7c4a9464ec4fe6!2z0L_RgNC-0YHQv9C10LrRgiDQmtGD0YDQvtGA0YLQvdC40LksIDI5LCDQmtC-0LHQu9C10LLQtSwg0JzQuNC60L7Qu9Cw0ZfQstGB0YzQutCwINC-0LHQu9Cw0YHRgtGMLCA1NzQyOA!5e1!3m2!1suk!2sua!4v1718457581724!5m2!1suk!2sua`
-    }]
-*/
